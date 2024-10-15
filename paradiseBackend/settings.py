7977a5220c397f45f3c2cc9c6c2e1a58c562a957
gitShubhamSingh <ptcsam74@gmail.com>
2024-10-15 17:19:42 +0530

    Update: latest UI

--- paradiseBackend/settings.py
+++ paradiseBackend/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build', 'static'),
+]
+
+MEDIA_URL = "uploads/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- paradiseBackend/urls.py
+++ paradiseBackend/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name="index.html")),
-
-]
+    path('',views.index, name="index")
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
